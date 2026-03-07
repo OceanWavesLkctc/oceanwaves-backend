aa8e7f18c9dd57d03fa0ae51ce40966151989ab8
snehakpr2005 <sneha2005kapoor@gmail.com>
2026-03-07 19:51:26 +0530

    from sneha

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-# oceanwaves-backend
-=======
 # oceanwaves-backend 
->>>>>>> f7e12119c7b4eaa8bb51e04d7e60bce49f0234e6
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,6 @@
-//authController.js
+// authController.js
 import userModel from "../models/User.js";
 import bcrypt from "bcryptjs";
-
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,12 +10,13 @@ export const signup = async (req, res) => {
 
     try {
         const { name, email, password, rollnumber, course, phonenumber, role } = req.body;
+
         const exist = await userModel.findOne({ email });
         if (exist) {
-            return res.status(400).json({ message: "user already exist" });
+            return res.status(400).json({ message: "User already exists" });
         }
-        const rollExist = await userModel.findOne({ rollnumber });
 
+        const rollExist = await userModel.findOne({ rollnumber });
         if (rollExist) {
             return res.status(400).json({
                 message: "Roll number already exists"
@@ -25,7 +25,7 @@ export const signup = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const UserModel = new userModel({
+        const newUser = new userModel({
             name,
             email,
             password: hashedPassword,
@@ -35,10 +35,16 @@ export const signup = async (req, res) => {
             role
         });
 
-        await UserModel.save();
-    }
-    catch (err) {
+        await newUser.save();
+
+        // IMPORTANT RESPONSE
+        res.status(201).json({
+            message: "User registered successfully",
+            user: newUser
+        });
+
+    } catch (err) {
         console.log("Signup Error:", err);
-        res.status(500).json({ message: "error reported" });
+        res.status(500).json({ message: "Error occurred" });
     }
 };
